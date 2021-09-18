de2880d07084a64ad6de175a09a4f01a7ccc6e1e
Server3055-std2020 <Winteryan1998@gmail.com>
2021-09-18 16:59:59 +0800

    add basic_transform in transform/ and add warpper

---
+++ transform/basic_transform/__init__.py
@@ -0,0 +1,4 @@
+from . import warpper
+
+__all__ = []
+__all__.extend(warpper.__all__)
---
+++ transform/basic_transform/warpper.py
@@ -0,0 +1,53 @@
+from typing import Callable, List
+import numpy as np
+
+__all__ = ['Iterator', 'Selector', 'Switch']
+
+
+class Iterator(object):
+    def __init__(self, transforms: List[Callable]) -> None:
+        super().__init__()
+        self.transforms = transforms
+
+    def __call__(self, input_data):
+        output_data = input_data
+
+        for transform in self.transforms:
+            output_data = transform(output_data)
+
+        return output_data
+
+
+class Selector(object):
+    def __init__(self,
+                 transforms: List[Callable],
+                 weights: List[int, float] = None) -> None:
+        super().__init__()
+        self.transforms = transforms
+
+        if weights == None:
+            self.weights = [1 / len(self.transforms)] * len(self.transforms)
+        else:
+            if len(weights) != len(self.transforms):
+                msg = 'the length of transforms and the length of weight should be the same.'
+                raise ValueError(msg)
+            else:
+                self.weights = weights
+
+    def __call__(self, input_data):
+        p = np.array(self.weights) / sum(self.weights)
+        index = np.random.choice(len(self.transforms), replace=False, p=p)
+        output_data = self.transforms[index](input_data)
+        return output_data
+
+
+class Switch(object):
+    def __init__(self, transform: Callable, p: float = 1.0) -> None:
+        super().__init__()
+        self.transform = transform
+        self.p = max(0, min(1, p))
+
+    def __call__(self, input_data):
+        output_data = self.transform(
+            input_data) if np.random.rand() < self.p else input_data
+        return output_data
