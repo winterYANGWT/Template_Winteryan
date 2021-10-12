6c3ab18e5d89267d17e34ad0e36f3a66516f4874
Server3055-std2020 <Winteryan1998@gmail.com>
2021-10-12 17:36:34 +0800

    add preset config in abstract_config

--- config/abstract_config.py
+++ config/abstract_config.py
@@ -1,9 +1,9 @@
-from abc import ABC, abstractclassmethod
+from abc import ABC
 
-
-__all__=['AbstractConfig']
+__all__ = ['AbstractConfig']
 
 
 class AbstractConfig(ABC):
     def __init__(self) -> None:
         super().__init__()
+        self.PREFETCH_FACTOR = 2
