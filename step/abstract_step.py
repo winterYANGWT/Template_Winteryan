2694b71fddd64b4a06253a2634c91221be0a0b91
Server3055-std2020 <Winteryan1998@gmail.com>
2021-11-06 15:08:01 +0800

    add comments to abstract_step and make it easy to use

--- step/abstract_step.py
+++ step/abstract_step.py
@@ -1,82 +1,87 @@
-import torch
 from abc import ABC, abstractclassmethod
 
 
 class AbstractStep(ABC):
+    '''
+    These class method should be implemented by subclass:
+        `load_config`,
+        `load_data`,
+        `load_models`,
+        `load_optimizers`,
+        `load_criterions`,
+        `load_meters`,
+        `forward`,
+        `backward`
+    '''
     def __init__(self) -> None:
         super().__init__()
+        self.load_config()
+        self.load_datasets()
+        self.load_models()
+        self.load_optimizers()
+        self.load_criterions()
+        self.load_meters()
 
     @abstractclassmethod
-    def load_data(self) -> dict(torch.utils.data.DataLoader):
+    def load_config(self):
         '''
-        Load dataset.
+        Load config.
         '''
-        raise NotImplementedError(
-            'load_data should be implemented by subclass.')
+        msg = 'load_config should be implemented by subclass.'
+        raise NotImplementedError(msg)
+
+    @abstractclassmethod
+    def load_datasets(self):
+        '''
+        Load datasets. All datasets will be contained in member variable self.datasets(dict) and generate the corresponding member variable self.data_loaders(dict).
+        '''
+        msg = 'load_datasets should be implemented by subclass.'
+        raise NotImplementedError(msg)
 
     @abstractclassmethod
     def load_models(self):
         '''
         Load models.
         '''
-        raise NotImplementedError(
-            'load_model should be implemented by subclass.')
+        msg = 'load_model should be implemented by subclass.'
+        raise NotImplementedError(msg)
 
     @abstractclassmethod
     def load_optimizers(self):
         '''
         Load optimizers.
         '''
-        raise NotImplementedError(
-            'load_optimizers should be implemented by subclass.')
+        msg = 'load_optimizers should be implemented by subclass.'
+        raise NotImplementedError(msg)
 
     @abstractclassmethod
     def load_criterions(self):
         '''
         load criterions.
         '''
-        raise NotImplementedError(
-            'load_criterions should be implemented by subclass.')
+        msg = 'load_criterions should be implemented by subclass.'
+        raise NotImplementedError(msg)
 
     @abstractclassmethod
     def load_meters(self):
         '''
         load meters.
         '''
-        raise NotImplementedError(
-            'load_meters should be implemented by subclass.')
+        msg = 'load_meters should be implemented by subclass.'
+        raise NotImplementedError(msg)
 
     @abstractclassmethod
     def forward(self, input_data):
         '''
         forward inference.
         '''
-        raise NotImplementedError('forward should be implemented by subclass.')
+        msg = 'forward should be implemented by subclass.'
+        raise NotImplementedError(msg)
 
     @abstractclassmethod
     def backward(self, input_data, output_data):
         '''
         backward propagation.
         '''
-        raise NotImplementedError(
-            'backward should be implemented by subclass.')
-
-    @property
-    def data_loaders(self):
-        return self._data_loaders
-
-    @property
-    def models(self):
-        return self._models
-
-    @property
-    def criterions(self):
-        return self._criterions
-
-    @property
-    def optimizers(self):
-        return self._optimizers
-
-    @property
-    def meters(self):
-        return self._meters
+        msg = 'backward should be implemented by subclass.'
+        raise NotImplementedError(msg)
