65b8b1564aeee76b4f76869785b6bc811ca30a01
yangwentao <Winteryan1998@gmail.com>
2022-03-08 01:30:56 +0800

    change train

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 from step import TrainStep
 import torch
+import os.path as path
 
 step = TrainStep()
-torch.set_num_threads(step.cfg.NUM_THREADS)
 torch.manual_seed(step.cfg.SEED)
 
-for epoch in range(step.cfg.START_EPOCH, step.cfg.EPOCHES):
+for epoch in range(step.cfg.START_EPOCH, step.cfg.EPOCHES + 1):
     step.train(epoch)
-    step.save_models(str(epoch))
+    step.save_models(path.join(step.cfg.MODEL_DIR, str(epoch)))
     step.val(epoch)
-    step.save_models('best')
+    step.save_models(path.join(step.cfg.MODEL_DIR, 'best'))
